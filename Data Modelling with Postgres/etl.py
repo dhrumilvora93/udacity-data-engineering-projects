f357248b64d179fd17d2e9b62b85cd4ec26f7965
meetparekh-cbi <meet.parekh@cbinsights.com>
2020-06-28 21:46:04 -0400

    added docker compose

--- Data Modelling with Postgres/etl.py
+++ Data Modelling with Postgres/etl.py
@@ -13,14 +13,16 @@ def process_song_file(cur, filepath):
     filepath:path_to_song_file
     """
     # open song file
-    df = pd.read_json(filepath,lines=True)
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = list(df[['song_id','title','artist_id','year','duration']].values)
+    song_data = list(df[["song_id", "title", "artist_id", "year", "duration"]].values)
     cur.executemany(song_table_insert, song_data)
-    
+
     # insert artist record
-    artist_data = list(df[['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']].values)
+    artist_data = list(
+        df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]].values
+    )
     cur.executemany(artist_table_insert, artist_data)
 
 
@@ -33,24 +35,24 @@ def process_log_file(cur, filepath):
     filepath:path_to_log_file
     """
     # open log file
-    df = pd.read_json(filepath,lines=True)
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = df.loc[df.page=='NextSong']
+    df = df.loc[df.page == "NextSong"]
 
     # convert timestamp column to datetime
-    t =  pd.to_datetime(df.ts, unit='ms')
-    
+    t = pd.to_datetime(df.ts, unit="ms")
+
     # insert time data records
     time_data = [t, t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday]
-    column_labels = ['timestamp', 'hour', 'day', 'week of year', 'month', 'year', 'weekday']
-    time_df = pd.DataFrame(dict(zip(column_labels,time_data)))
+    column_labels = ["timestamp", "hour", "day", "week of year", "month", "year", "weekday"]
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = df[['userId','firstName','lastName','gender','level']]
+    user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -58,9 +60,9 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
-        #print(row.song, row.artist, row.length)
+        # print(row.song, row.artist, row.length)
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
         if results:
@@ -71,8 +73,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = (row.ts, row.userId, row.level, songid, artistid \
-                    , row.sessionId, row.location, row.userAgent)
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
@@ -80,27 +81,27 @@ def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, "*.json"))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
     num_files = len(all_files)
-    print('{} files found in {}'.format(num_files, filepath))
+    print("{} files found in {}".format(num_files, filepath))
 
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
         conn.commit()
-        print('{}/{} files processed.'.format(i, num_files))
+        print("{}/{} files processed.".format(i, num_files))
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=db dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    process_data(cur, conn, filepath="data/song_data", func=process_song_file)
+    process_data(cur, conn, filepath="data/log_data", func=process_log_file)
 
     conn.close()
 
---
+++ Data Modelling with Postgres/run.py
@@ -0,0 +1,5 @@
+from create_tables import main as ct_main
+from etl import main as etl_main
+
+ct_main()
+etl_main()
